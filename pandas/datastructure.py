d044d181e24c9b013d707be520ab61aa9332cc66
Aishwarya <thakuraishwarya943@gmail.com>
2026-02-23 15:20:27 +0530

    .

--- pandas/datastructure.py
+++ pandas/datastructure.py
@@ -17,3 +17,15 @@ print(myvar[0])
 a = [1, 7, 2] 
 myvar = pd.Series(a, index = ["x", "y", "z"]) 
 print(myvar) 
+
+import numpy as np 
+array1 = np.array([6, 7, 8, 10, 13]) 
+array2 = np.array([6, 7, 8, 10, 13], dtype=np.int32) 
+print(array1.itemsize)   
+print(array2.itemsize)   
+
+import numpy as np 
+array1 = np.array([6, 7, 8]) 
+array2 = np.array([[1, 2, 3],[6, 7, 8]]) 
+print("\nData of array1 is: ",array1.data) 
+print("Data of array2 is: ",array2.data) 
